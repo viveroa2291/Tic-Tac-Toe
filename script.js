84a821041811831df2ba57dba7cb0dba9cb95687
viveroa2291 <viveroa2291@uwec.edu>
2023-08-13 23:29:36 -0500

    Hover Added
    Added the hover feature where it would hover the play as a way of knowing which player is playing.

--- script.js
+++ script.js
@@ -9,25 +9,54 @@ var bottom_middle = document.getElementById("bottom-middle");
 var bottom_right = document.getElementById("bottom-right");
 
 var top_left_value = document.getElementById("top-left-value");
+var topLeftFake = document.getElementById("top-left-value-fake");
 var top_middle_value = document.getElementById("top-middle-value");
+var topMiddleFake = document.getElementById("top-middle-value-fake");
 var top_right_value = document.getElementById("top-right-value");
+var topRightFake = document.getElementById("top-right-value-fake");
 
 var middle_left_value = document.getElementById("middle-left-value");
+var middleLeftFake = document.getElementById("middle-left-value-fake");
 var middle_value = document.getElementById("middle-value");
+var middleFake = document.getElementById("middle-value-fake");
 var middle_right_value = document.getElementById("middle-right-value");
+var middleRightFake = document.getElementById("middle-right-value-fake");
 
 var bottom_left_value = document.getElementById("bottom-left-value");
+var bottomLeftFake = document.getElementById("bottom-left-value-fake");
 var bottom_middle_value = document.getElementById("bottom-middle-value");
+var bottomMiddleFake = document.getElementById("bottom-middle-value-fake");
 var bottom_right_value = document.getElementById("bottom-right-value");
+var bottomRightFake = document.getElementById("bottom-right-value-fake");
 
 var winner = document.getElementById("winner");
 
 let count = 1;
-
+let fakeCount = 1;
+function topLeftPlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        top_left_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        top_left_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function topLeftHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+         top_left_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        top_left_value.innerHTML = "O";
+    } 
+}
 function topLeftPlay() {
     if (count % 2 == 1) {
         // odd number of clicks
-         top_left_value.innerHTML = "&#10060";
+        topLeftFake.innerHTML = "X";
          count++;
     } else {
         // even number of clicks
@@ -35,75 +64,235 @@ function topLeftPlay() {
         count++;
     }
 }
+function topMiddlePlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        top_middle_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        top_middle_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function topMiddleHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+         top_middle_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        top_middle_value.innerHTML = "O";
+    } 
+}
 function topMiddlePlay() {
     if (count % 2 == 1) {
-         top_middle_value.innerHTML = "&#10060"; 
+         topMiddleFake.innerHTML = "X"; 
          count++;
     } else {
-        top_middle_value.innerHTML = "O"; 
+        topMiddleFake.innerHTML = "O"; 
         count++;
+    } 
+}
+function topRightPlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        top_right_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        top_right_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function topRightHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+         top_right_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        top_right_value.innerHTML = "O";
     } 
 }
 function topRightPlay() {
     if (count % 2 == 1) {
-         top_right_value.innerHTML = "&#10060"; 
+         topRightFake.innerHTML = "X"; 
          count++;
     } else {
-        top_right_value.innerHTML = "O"; 
+        topRightFake.innerHTML = "O"; 
         count++;
+    } 
+}
+function middleLeftPlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        middle_left_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        middle_left_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function middleLeftHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+        middle_left_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        middle_left_value.innerHTML = "O";
     } 
 }
 function middleLeftPlay() {
     if (count % 2 == 1) {
-         middle_left_value.innerHTML = "&#10060"; 
+         middleLeftFake.innerHTML = "X"; 
          count++;
     } else {
-        middle_left_value.innerHTML = "O"; 
+        middleFake.innerHTML = "O"; 
         count++;
+    } 
+}
+function middlePlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        middle_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        middle_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function middleHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+        middle_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        middle_value.innerHTML = "O";
     } 
 }
 function middlePlay() {
     if (count % 2 == 1) {
-         middle_value.innerHTML = "&#10060"; 
+         middleFake.innerHTML = "X"; 
          count++;
     } else {
-        middle_value.innerHTML = "O"; 
+        middleFake.innerHTML = "O"; 
         count++;
+    } 
+}
+function middleRightPlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        middle_right_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        middle_right_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function middleRightHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+        middle_right_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        middle_right_value.innerHTML = "O";
     } 
 }
 function middleRightPlay() {
     if (count % 2 == 1) {
-         middle_right_value.innerHTML = "&#10060"; 
+         middleRightFake.innerHTML = "X"; 
          count++;e
     } else {
-        middle_right_value.innerHTML = "O"; 
+        middleRightFake.innerHTML = "O"; 
         count++;
+    } 
+}
+function bottomLeftPlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        bottom_left_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        bottom_left_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function bottomLeftHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+        bottom_left_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        bottom_left_value.innerHTML = "O";
     } 
 }
 function bottomLeftPlay() {
     if (count % 2 == 1) {
-        bottom_left_value.innerHTML = "&#10060"; 
+        bottomLeftFake.innerHTML = "X"; 
         count++;
     } else {
-        bottom_left_value.innerHTML = "O"; 
+        bottomLeftFake.innerHTML = "O"; 
         count++;
+    } 
+}
+function bottomMiddlePlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        bottom_middle_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        bottom_middle_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function bottomMiddleHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+        bottom_middle_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        bottom_middle_value.innerHTML = "O";
     } 
 }
 function bottomMiddlePlay() {
     if (count % 2 == 1) {
-        bottom_middle_value.innerHTML = "&#10060"; 
+        bottomMiddleFake.innerHTML = "X"; 
         count++;
     } else {
-        bottom_middle_value.innerHTML = "O"; 
+        bottomMiddleFake.innerHTML = "O"; 
         count++;
+    } 
+}
+function bottomRightPlayOff() {
+    if (fakeCount % 2 == 1) {
+        // odd number of clicks
+        bottom_right_value.innerHTML = ""; 
+        fakeCount++;
+    } else {
+        // even number of clicks
+        bottom_right_value.innerHTML = "";
+        fakeCount++;
+    }
+}
+function bottomRightHover() {
+    if (count % 2 == 1) {
+        // odd number of clicks
+        bottom_right_value.innerHTML = "X";
+    } else {
+        // even number of clicks
+        bottom_right_value.innerHTML = "O";
     } 
 }
 function bottomRightPlay() {
     if (count % 2 == 1) {
-        bottom_right_value.innerHTML = "&#10060"; 
+        bottomRightFake.innerHTML = "X"; 
         count++;
     } else {
-        bottom_right_value.innerHTML = "O"; 
+        bottomRightFake.innerHTML = "O"; 
         count++;
     } 
 }
